65dc144fc4b8a24a5c951331cf8fefa64db423a0
Kennylovetheworld <944545895@qq.com>
2019-12-07 00:28:31 +0000

    m3

---
+++ adlist/adlist/github_settings.py
@@ -0,0 +1,46 @@
+# Copy this file to github_settings.py (don't check it into github)
+
+# Go to https://github.com/settings/developers
+
+# Add a New OAuth2 App
+
+# Using ngrok is hard because the url changes every time you start ngrok
+
+# If you are running on localhost, here are some settings:
+
+# Application name: ChuckList Local
+# Homepage Url: http://localhost:8000
+# Application Description: Whatever
+# Authorization callback URL: http://127.0.0.1:8000/oauth/complete/github/
+
+
+# Using PythonAnywhere here are some settings:
+
+# Application name: ChuckList PythonAnywhere
+# Homepage Url: https://drchuck.pythonanywhere.com
+# Application Description: Whatever
+# Authorization callback URL: https://drchuck.pythonanywhere.com/oauth/complete/github/
+
+# Also on PythonAnywhere, go into the Web tab and enable "Force HTTPS"
+# so you don't get a redirect URI mismatch.
+
+# Then copy the client_key and secret to this file
+
+SOCIAL_AUTH_GITHUB_KEY = 'ac7bf679d6d3faede83c'
+SOCIAL_AUTH_GITHUB_SECRET = '4c578eedabccadb55f5530137df9c2b596cfa2e0'
+
+# The code below dynamically switches between non-social login.html
+# and social_login.html when we notice that social login has been
+# configured in settings.py (later in the course)
+# Or just uncomment the path above when you enable social login
+
+from django.contrib.auth import views as auth_views
+try:
+    from . import github_settings
+    social_login = 'registration/login_social.html'
+    urlpatterns.insert(0,
+        path('accounts/login/', auth_views.LoginView.as_view(template_name=social_login))
+    )
+    print('Using',social_login,'as the login template')
+except:
+    print('Using registration/login.html as the login template')
--- adlist/ads/views.py
+++ adlist/ads/views.py
@@ -6,7 +6,7 @@ from django.core.files.uploadedfile import InMemoryUploadedFile
 from django.urls import reverse_lazy
 from django.http import HttpResponse
 from ads.owner import OwnerListView, OwnerDetailView, OwnerCreateView, OwnerUpdateView, OwnerDeleteView
-from ads.models import Ad, Comment
+from ads.models import Ad, Comment, Fav
 from ads.forms import CreateForm
 from ads.forms import CommentForm
 from django.urls import reverse
@@ -14,6 +14,15 @@ from django.urls import reverse
 class AdListView(OwnerListView):
     model = Ad
     template_name = "ads/ads_list.html"
+    def get(self, request) :
+        ad_list = Ad.objects.all()
+        favorites = list()
+        if request.user.is_authenticated:
+            # rows = [{'id': 2}]  (A list of rows)
+            rows = request.user.favorite_ads.values('id')
+            favorites = [ row['id'] for row in rows ]
+        ctx = {'ad_list' : ad_list, 'favorites': favorites}
+        return render(request, self.template_name, ctx)
 
 class AdDetailView(OwnerDetailView):
     model = Ad
@@ -106,3 +115,32 @@ class CommentDeleteView(OwnerDeleteView):
     def get_success_url(self):
         ad = self.object.ad
         return reverse('ads:ad_detail', args=[ad.id])
+
+# https://stackoverflow.com/questions/16458166/how-to-disable-djangos-csrf-validation
+from django.views.decorators.csrf import csrf_exempt
+from django.utils.decorators import method_decorator
+from django.db.utils import IntegrityError
+
+@method_decorator(csrf_exempt, name='dispatch')
+class AddFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Add PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        fav = Fav(user=request.user, ad=t)
+        try:
+            fav.save()  # In case of duplicate key
+        except IntegrityError as e:
+            pass
+        return HttpResponse()
+
+@method_decorator(csrf_exempt, name='dispatch')
+class DeleteFavoriteView(LoginRequiredMixin, View):
+    def post(self, request, pk) :
+        print("Delete PK",pk)
+        t = get_object_or_404(Ad, id=pk)
+        try:
+            fav = Fav.objects.get(user=request.user, ad=t).delete()
+        except Fav.DoesNotExist as e:
+            pass
+
+        return HttpResponse()
